821f97b0b487f074748c5f933ae9018de9dfe5e8
kmsdhanajayan <103888986+kmsdhanajayan@users.noreply.github.com>
2022-09-01 23:21:44 +0530

    modified palindrome function

--- src/Question/Palindrome.java
+++ src/Question/Palindrome.java
@@ -7,21 +7,29 @@ public class Palindrome {
 	public static void main(String args[]) {
 		String str,
 		rev = "";
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter a string:");
-		str = sc.nextLine();
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a string:");
+			str = sc.nextLine();
+		}
 
 		int length = str.length();
+		
+		for(int i=0 ; i< length/2 ; i++) {
+			if(str.charAt(i)!= str.charAt(length-i-1)) {
+				System.out.println(str +" is not plaindrome");
+				return;
+			}
+		}
+		System.out.println(str + " is a palindrome");
 
-		for (int i = length - 1; i >= 0; i--)
-			
-			rev = rev + str.charAt(i);
-
-		if (str.equals(rev))
-			System.out.println(str + " is a palindrome");
-		else
-			System.out.println(str + " is not a palindrome");
+		/*
+		 * for (int i = length - 1; i >= 0; i--)
+		 * 
+		 * rev = rev + str.charAt(i);
+		 * 
+		 * if (str.equals(rev)) System.out.println(str + " is a palindrome"); else
+		 * System.out.println(str + " is not a palindrome");
+		 */
 
 	}
 
